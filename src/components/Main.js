40e12d3b7cd67d845feeed18bddc77faf692708c
kai <zk.zhengkai@outlook.com>
2020-05-04 23:14:01 -0400

    add Main.js

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Profile from "./Profile";
+class Main extends Component {
+    render() {
+        return (
+            <div className='main'>
+                <Profile />
+            </div>
+        );
+    }
+}
+
+export default Main;
