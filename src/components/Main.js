439c31023b39165f03fb97f805c367a3cd895bfe
kai <zk.zhengkai@outlook.com>
2020-05-05 00:23:57 -0400

    get playerInfo

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,36 @@
 import React, {Component} from 'react';
 import Profile from "./Profile";
+import ShotChart from "./ShotChart";
+import nba from '../nba-client'
+
+
 class Main extends Component {
+    state = {
+        playerInfo: {}
+    }
+
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         playerInfo: {}
+    //     }
+    // }
+    componentDidMount() {
+        window.nba = nba;
+        nba.stats.playerInfo({PlayerID: (nba.findPlayer('Stephen Curry')).playerId})
+            .then(info => {
+                console.log(info);
+                const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+                console.log(playInfo);
+                this.setState({ playerInfo: playInfo });
+            })
+    }
+
     render() {
         return (
             <div className='main'>
-                <Profile />
+                <Profile playerInfo = {this.state.playerInfo}/>
+                <ShotChart />
             </div>
         );
     }
