991faf57ef37bcfa646d24a505527ea4f851ffcf
kai <zk.zhengkai@outlook.com>
2020-05-06 22:48:48 -0400

    add counter slider

---
+++ src/components/CounterSlider.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class CounterSlider extends Component {
+    render() {
+        return (
+            <div>
+                slider
+            </div>
+        );
+    }
+}
+
+export default CounterSlider;
